a067ee4434e55d6771c516445e0e0fa1608fe260
김대성 <daesung3725@naver.com>
2024-10-08 16:41:04 +0900

    [#6]feat: SoundData 및 type 정의

---
+++ src/lib/sound-data.ts
@@ -0,0 +1,40 @@
+import { SoundType } from './type';
+
+export const SoundData: SoundType[] = [
+  {
+    id: 1,
+    name: 'Duck Sound',
+    imgUrl: '/images/duck.svg',
+    url: '/audio/duck.mp3',
+  },
+  {
+    id: 2,
+    name: 'gold bell',
+    imgUrl: '/images/bell.svg',
+    url: '/audio/bell.mp3',
+  },
+  {
+    id: 3,
+    name: 'hand clap',
+    imgUrl: '/images/handClap.svg',
+    url: '/audio/handClap.mp3',
+  },
+  {
+    id: 4,
+    name: 'airhorn',
+    imgUrl: '/images/horn.svg',
+    url: '/audio/horn.mp3',
+  },
+  {
+    id: 5,
+    name: 'sad horn',
+    imgUrl: '/images/trumpet.svg',
+    url: '/audio/trumpet.mp3',
+  },
+  {
+    id: 6,
+    name: 'ba dum tss',
+    imgUrl: '/images/bassDrum.svg',
+    url: '/audio/bassDrum.mp3',
+  },
+];
