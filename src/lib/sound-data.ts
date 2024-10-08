4bc5a3349eaba705dd826e0ac70807d04e3b7494
김대성 <daesung3725@naver.com>
2024-10-08 19:45:42 +0900

    [#6]build: 용량이 큰 이미지와 mp3 파일을 s3에 올림

--- src/lib/sound-data.ts
+++ src/lib/sound-data.ts
@@ -4,37 +4,40 @@ export const SoundData: SoundType[] = [
   {
     id: 1,
     name: 'Duck Sound',
-    imgUrl: '/images/duck.svg',
-    url: '/audio/duck.mp3',
+    imgUrl: 'https://aws-file-uploder.s3.ap-northeast-2.amazonaws.com/duck.svg',
+    url: 'https://aws-file-uploder.s3.ap-northeast-2.amazonaws.com/duck.mp3',
   },
   {
     id: 2,
     name: 'gold bell',
-    imgUrl: '/images/bell.svg',
-    url: '/audio/bell.mp3',
+    imgUrl: 'https://aws-file-uploder.s3.ap-northeast-2.amazonaws.com/bell.svg',
+    url: 'https://aws-file-uploder.s3.ap-northeast-2.amazonaws.com/bell.mp3',
   },
   {
     id: 3,
     name: 'hand clap',
-    imgUrl: '/images/handClap.svg',
-    url: '/audio/handClap.mp3',
+    imgUrl:
+      'https://aws-file-uploder.s3.ap-northeast-2.amazonaws.com/handClap.svg',
+    url: 'https://aws-file-uploder.s3.ap-northeast-2.amazonaws.com/handClap.mp3',
   },
   {
     id: 4,
     name: 'airhorn',
-    imgUrl: '/images/horn.svg',
-    url: '/audio/horn.mp3',
+    imgUrl: 'https://aws-file-uploder.s3.ap-northeast-2.amazonaws.com/horn.svg',
+    url: 'https://aws-file-uploder.s3.ap-northeast-2.amazonaws.com/horn.mp3',
   },
   {
     id: 5,
     name: 'sad horn',
-    imgUrl: '/images/trumpet.svg',
-    url: '/audio/trumpet.mp3',
+    imgUrl:
+      'https://aws-file-uploder.s3.ap-northeast-2.amazonaws.com/trumpet.svg',
+    url: 'https://aws-file-uploder.s3.ap-northeast-2.amazonaws.com/trumpet.mp3',
   },
   {
     id: 6,
     name: 'ba dum tss',
-    imgUrl: '/images/bassDrum.svg',
-    url: '/audio/bassDrum.mp3',
+    imgUrl:
+      'https://aws-file-uploder.s3.ap-northeast-2.amazonaws.com/bassDrum.svg',
+    url: 'https://aws-file-uploder.s3.ap-northeast-2.amazonaws.com/bassDrum.mp3',
   },
 ];
