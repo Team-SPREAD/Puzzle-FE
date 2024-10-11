d1b3efc381feca6ec60361d64eae414d947e1342
김대성 <daesung3725@naver.com>
2024-10-11 13:15:57 +0900

    refactor: 파일명 변경

---
+++ src/lib/types.ts
@@ -0,0 +1,18 @@
+export type SoundType = {
+  id: number;
+  name: string;
+  imgUrl: string;
+  url: string;
+};
+export type UserInfo = {
+  id: string;
+  name: string;
+  avatar: string;
+  email: string;
+};
+export type ProjectInfo = {
+  name: string;
+  id: string;
+  description: string;
+  role: string;
+};
